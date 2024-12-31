0dd4d2039bf9ffce7b8a8389fadb9d593a5f2e38
umaxcode <maxwellodoom1729@gmail.com>
2024-12-31 14:02:50 +0000

    feat: import controllers to speed up cold starts

--- src/main/java/org/umaxcode/Application.java
+++ src/main/java/org/umaxcode/Application.java
@@ -3,14 +3,15 @@ package org.umaxcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-
 import org.umaxcode.controller.PingController;
+import org.umaxcode.controller.TaskManagementController;
+import org.umaxcode.controller.UserAuthController;
 
 
 @SpringBootApplication
 // We use direct @Import instead of @ComponentScan to speed up cold starts
 // @ComponentScan(basePackages = "org.umaxcode.controller")
-@Import({ PingController.class })
+@Import({PingController.class, TaskManagementController.class, UserAuthController.class})
 public class Application {
 
     public static void main(String[] args) {

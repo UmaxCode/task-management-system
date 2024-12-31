688644ffcdfcceb14560007cd699b2f229d13af2
umaxcode <maxwellodoom1729@gmail.com>
2024-12-31 15:37:04 +0000

    feat: add sns configurations

--- src/main/java/org/umaxcode/config/AWSConfig.java
+++ src/main/java/org/umaxcode/config/AWSConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.sns.SnsClient;
 
 @Configuration
 public class AWSConfig {
@@ -16,6 +17,13 @@ public class AWSConfig {
     public CognitoIdentityProviderClient cognitoClient() {
         return CognitoIdentityProviderClient.builder()
                 .region(Region.of(awsRegion)) // Replace YOUR_REGION
+                .build();
+    }
+
+    @Bean
+    public SnsClient snsClient() {
+        return SnsClient.builder()
+                .region(Region.of(awsRegion)) // Set your region
                 .build();
     }
 }

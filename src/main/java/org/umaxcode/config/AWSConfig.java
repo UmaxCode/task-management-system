c02b4b31efe3f9c7e6d3eba0807682d8d3e5c62f
umaxcode <maxwellodoom1729@gmail.com>
2025-01-02 16:31:37 +0000

    feat: add dynamodb config and update table id

--- src/main/java/org/umaxcode/config/AWSConfig.java
+++ src/main/java/org/umaxcode/config/AWSConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cognitoidentityprovider.CognitoIdentityProviderClient;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.sns.SnsClient;
 
@@ -31,6 +32,13 @@ public class AWSConfig {
     @Bean
     public LambdaClient lambdaClient(){
         return LambdaClient.builder()
+                .region(Region.of(awsRegion))
+                .build();
+    }
+
+    @Bean
+    public DynamoDbClient dynamoDbClient(){
+        return DynamoDbClient.builder()
                 .region(Region.of(awsRegion))
                 .build();
     }

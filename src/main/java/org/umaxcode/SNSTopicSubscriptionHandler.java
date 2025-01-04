b78ef99a64c5d98eee3fc583f511e3b55c20ee60
umaxcode <maxwellodoom1729@gmail.com>
2025-01-04 14:54:11 +0000

    feat: add filtering policy to sns subscription

--- src/main/java/org/umaxcode/SNSTopicSubscriptionHandler.java
+++ src/main/java/org/umaxcode/SNSTopicSubscriptionHandler.java
@@ -3,6 +3,7 @@ package org.umaxcode;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.SetSubscriptionAttributesRequest;
 import software.amazon.awssdk.services.sns.model.SubscribeRequest;
 import software.amazon.awssdk.services.sns.model.SubscribeResponse;
 
@@ -30,13 +31,32 @@ public class SNSTopicSubscriptionHandler implements RequestHandler<Map<String, S
         SubscribeRequest subscribeRequest = SubscribeRequest.builder()
                 .protocol("email")  // Protocol is email to receive notifications
                 .endpoint(userEmail)  // User's email address to subscribe
+                .returnSubscriptionArn(true)
                 .topicArn(snsTopicArn)  // SNS Topic ARN
                 .build();
 
         try {
             SubscribeResponse response = snsClient.subscribe(subscribeRequest);
             context.getLogger().log("Subscription result: " + response);
-            return "Successfully subscribed " + userEmail + " to the SNS topic: " + snsTopicArn;
+
+            // Define a filter policy
+            String filterPolicy = String.format("{ \"endpointEmail\": [\"%s\"] }", userEmail);
+
+            // Set the filter policy for the subscription
+            String subscriptionArn = response.subscriptionArn();
+
+            System.out.println("SubscriptionArn" + subscriptionArn);
+            SetSubscriptionAttributesRequest filterPolicyRequest = SetSubscriptionAttributesRequest.builder()
+                    .subscriptionArn(subscriptionArn)
+                    .attributeName("FilterPolicy")
+                    .attributeValue(filterPolicy)
+                    .build();
+
+            snsClient.setSubscriptionAttributes(filterPolicyRequest);
+
+            context.getLogger().log("Filter policy set for subscription: " + subscriptionArn);
+
+            return "Successfully subscribed " + userEmail + " to the SNS topic with filter policy: " + snsTopicArn;
         } catch (Exception e) {
             context.getLogger().log("Error subscribing user: " + e.getMessage());
             return "Failed to subscribe user to SNS topic.";

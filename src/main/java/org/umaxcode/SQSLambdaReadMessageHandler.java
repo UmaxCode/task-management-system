885c3d6cf4ceeacfbc89b039ef3cdd150ffcf477
umaxcode <maxwellodoom1729@gmail.com>
2025-01-04 23:50:58 +0000

    feat: update lambda handler to process message written to the queue (hit deadline/ approaching deadline)

--- src/main/java/org/umaxcode/SQSLambdaReadMessageHandler.java
+++ src/main/java/org/umaxcode/SQSLambdaReadMessageHandler.java
@@ -29,11 +29,23 @@ public class SQSLambdaReadMessageHandler implements RequestHandler<SQSEvent, Str
             System.out.println("Message body: " + messageBody);
             System.out.println("Message attributes: " + messageReason);
 
+            String receiver = message.getMessageAttributes().get("receiver").getStringValue();
+            String name = message.getMessageAttributes().get("name").getStringValue();
+            String description = message.getMessageAttributes().get("description").getStringValue();
+            String deadline = message.getMessageAttributes().get("deadline").getStringValue();
+            String topicArn = message.getMessageAttributes().get("topicArn").getStringValue();
+
             if ("task-creation".equals(messageReason)) {
-
                 System.out.println("Sending task creation notification");
-                sendTaskCreationNotification(message);
-
+                sendTaskNotification(receiver, name, description, deadline,
+                        topicArn, "New Task Assignment");
+            } else if ("task-hit-deadline".equals(messageReason)) {
+                System.out.println("Sending task deadline notification");
+                triggerStepFunction();
+            } else if ("task-approach-deadline".equals(messageReason)) {
+                System.out.println("Sending task approach deadline notification");
+                sendTaskNotification(receiver, name, description, deadline,
+                        topicArn, "Task Approach Deadline");
             } else {
                 context.getLogger().log("Invalid message reason");
             }
@@ -41,14 +53,9 @@ public class SQSLambdaReadMessageHandler implements RequestHandler<SQSEvent, Str
         return "Processed message successfully!";
     }
 
-    private void sendTaskCreationNotification(SQSEvent.SQSMessage message) {
-
-        String receiver = message.getMessageAttributes().get("receiver").getStringValue();
-        String name = message.getMessageAttributes().get("name").getStringValue();
-        String description = message.getMessageAttributes().get("description").getStringValue();
-        String deadline = message.getMessageAttributes().get("deadline").getStringValue();
-        String topicArn = message.getMessageAttributes().get("topicArn").getStringValue();
-
+    private void sendTaskNotification(String receiver, String name, String description,
+                                      String deadline, String topicArn, String title
+    ) {
         // Create message attributes sns filtering
         Map<String, MessageAttributeValue> messageAttributes = new HashMap<>();
         messageAttributes.put("endpointEmail", MessageAttributeValue.builder()
@@ -60,7 +67,7 @@ public class SQSLambdaReadMessageHandler implements RequestHandler<SQSEvent, Str
 
         PublishRequest publishRequest = PublishRequest.builder()
                 .topicArn(topicArn)
-                .subject("New Task Assignment")
+                .subject(title)
                 .message(messageContent)
                 .messageAttributes(messageAttributes)
                 .build();
@@ -72,7 +79,10 @@ public class SQSLambdaReadMessageHandler implements RequestHandler<SQSEvent, Str
         } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
+    private void triggerStepFunction() {
+
+        System.out.println("Step function triggered");
+    }
 }

e22d10bd6f1b374b63260dae6abb55d3923ab4e1
umaxcode <maxwellodoom1729@gmail.com>
2025-01-17 16:13:53 +0000

    fix: fix resource not found response status

--- src/main/java/org/umaxcode/config/SecurityConfig.java
+++ src/main/java/org/umaxcode/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig {
                 .cors(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/ping").permitAll()
+                        .requestMatchers("/ping", "/pong").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt
